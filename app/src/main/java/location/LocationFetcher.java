e24f07e32f0e6ea85d04f3b9e4b3429f5f926311
BPYap <32807097+BPYap@users.noreply.github.com>
2018-03-26 21:20:53 +0800

    Moved package directory

--- app/src/main/java/location/LocationFetcher.java
+++ app/src/main/java/location/LocationFetcher.java
@@ -1,7 +1,8 @@
+package location;
+
 import android.Manifest;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -11,29 +12,25 @@ import com.google.android.gms.location.*;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 
-import java.util.Locale;
-
 public final class LocationFetcher {
-    public static LocationCallback mLocationCallback;
-    public static Location location;
-
     private static LocationFetcher single_instance = null;
 
     // request code mappings
-    private static final int LOCATION_PERMISSION_REQUEST = 0;
-    private static final int LOCATION_SETTING_REQUEST = 1;
+    public static final int LOCATION_PERMISSION_REQUEST = 0;
+    public static final int LOCATION_SETTING_REQUEST = 1;
 
     // Location settings
     private LocationRequest mLocationRequest;
     private LocationSettingsRequest.Builder location_setting_request;
     private SettingsClient settings_client;
+    private LocationCallback mLocationCallback;
 
     // Location provider and callback handler
     private FusedLocationProviderClient mFusedLocationClient;
 
     private LocationFetcher(){}
 
-    private LocationFetcher(MainActivity mainActivity) {
+    private LocationFetcher(MainActivity mainActivity, LocationCallback locationCallback) {
         // Initialize LocationRequest object
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(30000);
@@ -47,58 +44,26 @@ public final class LocationFetcher {
 
         // Setup FusedLocationClient and define locationCallback
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mainActivity);
-        mLocationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-                location = locationResult.getLastLocation();
-            }
-        };
+
+        mLocationCallback = locationCallback;
     }
 
-    public static LocationFetcher getInstance(MainActivity mainActivity)
+    public static LocationFetcher getInstance(MainActivity mainActivity, LocationCallback locationCallback)
     {
         if (single_instance == null)
-            single_instance = new LocationFetcher(mainActivity);
+            single_instance = new LocationFetcher(mainActivity, locationCallback);
 
         return single_instance;
     }
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode,
-//                                           @NonNull String permissions[], @NonNull int[] grantResults) {
-//        switch (requestCode) {
-//            case LOCATION_PERMISSION_REQUEST:
-//                if (grantResults.length > 0
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    get_location_update();
-//                } else {
-//                    request_location_permission();
-//                }
-//        }
-//    }
 
     private void request_location_permission(MainActivity mainActivity) {
         if (ContextCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_DENIED) {
                 ActivityCompat.requestPermissions(mainActivity,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        LOCATION_SETTING_REQUEST);
+                        LOCATION_PERMISSION_REQUEST);
         }
     }
-
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        switch (requestCode) {
-//            case LOCATION_SETTING_REQUEST:
-//                if (resultCode == RESULT_OK) {
-//                    mDebugTextView.setText("High Accuracy GPS mode ON");
-//                    get_location_update();
-//                }
-//        }
-//    }
 
     private void check_location_settings(final MainActivity mainActivity) {
         Task<LocationSettingsResponse> task = settings_client.checkLocationSettings(location_setting_request.build());
@@ -120,7 +85,7 @@ public final class LocationFetcher {
         });
     }
 
-    private void get_location_update(MainActivity mainActivity) {
+    public void get_location_update(MainActivity mainActivity) {
         if (ContextCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             check_location_settings(mainActivity);
