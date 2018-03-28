f0be8cc56aec31d3adc71cbf782b764133033ac5
BPYap <32807097+BPYap@users.noreply.github.com>
2018-03-28 21:42:22 +0800

    Refactored codes

--- app/src/main/java/com/demo/locationupdatedemo/MainActivity.java
+++ app/src/main/java/com/demo/locationupdatedemo/MainActivity.java
@@ -1,10 +1,7 @@
 package com.demo.locationupdatedemo;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +9,7 @@ import android.widget.TextView;
 import location.LocationFetcher;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationResult;
+import android.util.Log;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -81,26 +79,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i("Main", "Resuming");
         mLocationFetcher.get_location_update(this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String permissions[], @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case LocationFetcher.LOCATION_PERMISSION_REQUEST:
-                mLocationFetcher.get_location_update(this);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case LocationFetcher.LOCATION_SETTING_REQUEST:
-                if (resultCode == RESULT_OK) {
-                    mDebugTextView.setText("High Accuracy GPS mode ON");
-                    mLocationFetcher.get_location_update(this);
-                }
-        }
+    protected void onPause() {
+        super.onPause();
+        Log.i("Main", "Paused");
     }
 }
--- app/src/main/java/location/LocationFetcher.java
+++ app/src/main/java/location/LocationFetcher.java
@@ -6,16 +6,18 @@ import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import com.demo.locationupdatedemo.MainActivity;
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.*;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
 public final class LocationFetcher {
     private static LocationFetcher single_instance = null;
 
-    // request code mappings
+    // request code
     public static final int LOCATION_PERMISSION_REQUEST = 0;
     public static final int LOCATION_SETTING_REQUEST = 1;
 
@@ -34,7 +36,7 @@ public final class LocationFetcher {
         // Initialize LocationRequest object
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(30000);
-        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setFastestInterval(1000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
         // Initialize settings request
@@ -56,30 +58,39 @@ public final class LocationFetcher {
         return single_instance;
     }
 
-    private void request_location_permission(MainActivity mainActivity) {
-        if (ContextCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_DENIED) {
-                ActivityCompat.requestPermissions(mainActivity,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        LOCATION_PERMISSION_REQUEST);
-        }
+    private void get_location_permission(MainActivity mainActivity) {
+        ActivityCompat.requestPermissions(mainActivity,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_REQUEST);
     }
 
     private void check_location_settings(final MainActivity mainActivity) {
         Task<LocationSettingsResponse> task = settings_client.checkLocationSettings(location_setting_request.build());
 
+        task.addOnSuccessListener(mainActivity, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                Log.i("LocationFetcher", "Location settings are satisfied");
+                Log.i("LocationFetcher", "Requesting location info from provider");
+                try {
+                    mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+                }
+                catch (SecurityException SecEx){
+                    Log.e("LocationFetcher", "Unexpected error occurred!", SecEx);
+                }
+            }
+        });
+
         task.addOnFailureListener(mainActivity, new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
                 if (e instanceof ResolvableApiException) {
+                    Log.i("LocationFetcher", "Location settings not satisfied");
                     try {
                         ResolvableApiException resolvable = (ResolvableApiException) e;
-                        resolvable.startResolutionForResult(mainActivity,
-                                LOCATION_SETTING_REQUEST);
+                        resolvable.startResolutionForResult(mainActivity, LOCATION_SETTING_REQUEST);
                     }
-                    catch (IntentSender.SendIntentException sendEx) {
-
-                    }
+                    catch (IntentSender.SendIntentException sendEx) {}
                 }
             }
         });
@@ -87,13 +98,13 @@ public final class LocationFetcher {
 
     public void get_location_update(MainActivity mainActivity) {
         if (ContextCompat.checkSelfPermission(mainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+                == PackageManager.PERMISSION_DENIED) {
+            Log.i("LocationFetcher", "No location permission");
+            get_location_permission(mainActivity);
+        } else {
+            Log.i("LocationFetcher", "Location permission obtained");
+            Log.i("LocationFetcher", "Checking location settings");
             check_location_settings(mainActivity);
-            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
-                    mLocationCallback, null);
-        }
-        else {
-            request_location_permission(mainActivity);
         }
     }
 }
